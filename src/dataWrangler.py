fb693bff15c9fa4f431e76199a619ca65f3248f6
hmmartiny <hmmartiny@hotmail.com>
2018-07-09 22:47:41 +0200

    Growth analysis part1

---
+++ src/dataWrangler.py
@@ -0,0 +1,74 @@
+#!/user/bin/python3
+
+import pandas as pd
+import numpy as np
+
+def PolyArea(x,y):
+    return 0.5*np.abs(np.dot(x,np.roll(y,1))-np.dot(y,np.roll(x,1)))
+def CircleArea(r):
+	return np.pi*np.square(r)
+def EllipseArea(rx, ry):
+	return np.pi * rx * ry
+
+# ANNOTATIONS MADE BY PROGRAM VIA
+# http://www.robots.ox.ac.uk/~vgg/software/via/
+org_df = pd.DataFrame.from_csv('../data/annotations.csv', sep = ",", index_col = None)
+
+# can drop file size, file attributes
+org_df = org_df.drop(['file_size', 'file_attributes', 'region_attributes'], axis = 1)
+
+mediaDict = []
+tempLightDict = []
+
+for i, row in org_df.iterrows():
+	fileSplit =  row["filename"].split('_')
+	date = fileSplit[0]
+	try:
+		attr = eval(row["region_shape_attributes"].replace('.', ','))
+		area = CircleArea(attr['r'])
+	except SyntaxError:
+		attr = eval(row["region_shape_attributes"].replace('.', ','))
+		area = CircleArea(attr['r'])
+	except KeyError:
+		if len(attr) == 0:
+			attr = eval(row['region_id'].replace('.', ','))
+			if "ellipse" in attr["name"]:
+				area = EllipseArea(attr["rx"], attr["ry"])
+			elif "circle" in attr["name"]:
+				area = CircleArea(attr['r'])
+		elif "polygon" in attr["name"]:
+			x = np.asarray(attr["all_points_x"])
+			y = np.asarray(attr["all_points_y"])
+			area = PolyArea(x, y)
+
+	if 'dark' in row["filename"] or 'light' in row["filename"]:
+		species = "".join(fileSplit[1])
+		temp = fileSplit[2]
+		light = fileSplit[3]
+		dupl = fileSplit[-1].replace('.jpg', '').replace('#', '')
+
+		tempLightDict.append({'species': species, 'temperature': temp, 'light_source': light, 'date': date, 'dupl': dupl, 'area': area})
+		# [species].append([temp, light, date, dupl, area])
+
+	else:
+		try:
+			medium = fileSplit[1]
+			conc = fileSplit[2]
+			species = "".join(fileSplit[3])
+			dupl = fileSplit[4].replace('.jpg', '').replace('#', '')
+		except IndexError:
+			conc = None
+			species = "".join(fileSplit[2])
+			dupl = 1
+
+		print(species, conc, medium)
+
+		mediaDict.append({'species': species, 'medium': medium, 'concentration': conc, 'date': date, 'dupl': dupl, 'area': area})
+		#[species].append([medium, conc, date, dupl, area])
+
+
+df1 = pd.DataFrame(mediaDict)
+df1.to_csv('../data/medium_growths.csv', sep = "\t")
+
+df2 = pd.DataFrame(tempLightDict)
+df1.to_csv('../data/tempLight_growths.csv', sep = "\t")
--- src/renaming.py
+++ src/renaming.py
@@ -14,10 +14,11 @@ else:
 dateDirs = os.listdir(pictureDir)
 dateDirs = [x for x in dateDirs if x.endswith("plates")] # only work with correct picture folders
 
+# MOVE PICTURES
 for dateDir in dateDirs:
 	date = datetime.datetime.strptime(dateDir.split(' ')[0], '%d-%m-%Y').strftime('%Y-%m-%d')
 
-	destDir = wd + '/data/pic/' + date + '/'
+	destDir = wd + '/data/pic/'
 
 	if not os.path.exists(destDir):
 	    os.makedirs(destDir)
@@ -29,4 +30,4 @@ for dateDir in dateDirs:
 	for pictureFile in pictureFiles:
 		print(pictureFile)
 
-		copyfile(pictureDir + dateDir +'/' + pictureFile,  destDir + pictureFile.replace(' ', '_'))
+		copyfile(pictureDir + dateDir +'/' + pictureFile,  destDir + date + '_' + pictureFile.replace(' ', '_'))
